a5858e185f235d8559d5569dad482f132595375d
Vatsal Raina <vatsalraina@hotmail.co.uk>
2021-02-25 00:54:47 +0000

    race

--- electra_multipleChoice/train_RACE.py
+++ electra_multipleChoice/train_RACE.py
@@ -105,7 +105,7 @@ def main(args):
             for opt in opts:
                 combo = context + " [SEP] " + question + " " + opt
                 inp_ids = tokenizer.encode(combo)
-                if len(inp_ids>512):
+                if len(inp_ids)>512:
                     inp_ids = inp_ids[-512:]
                 tok_type_ids = [0 if i<= inp_ids.index(102) else 1 for i in range(len(inp_ids))]
                 four_inp_ids.append(inp_ids)
